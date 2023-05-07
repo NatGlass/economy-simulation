c0916e15f2dabb014936aaf864f79a537e8df014
Nathan G <85080878+Visibyte@users.noreply.github.com>
2023-05-07 21:04:16 +0000

    calculate spending

--- src/models/Consumer.ts
+++ src/models/Consumer.ts
@@ -3,12 +3,16 @@ import IAgent from "./IAgent";
 export default class Consumer implements IAgent {
     income: number;
     spending: number;
+    propensityToConsume: number;
 
-    constructor(income: number, spending: number) {
+    constructor(income: number, spending: number, propensityToConsume: number) {
         this.income = income;
         this.spending = spending;
+        this.propensityToConsume = propensityToConsume;
     }
     update(): void {
-            // Update the consumer's behavior based on market conditions
+        // Update the consumer's behavior based on market conditions
+        this.calculateSpending();
     }
 }
+
