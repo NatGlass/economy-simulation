fa26e1b7766ed7349e15c850db816f44b7d67f2e
Nathan G <Nathanglass26@gmail.com>
2023-05-08 10:18:36 +0100

    calculate spending

--- src/models/Consumer.ts
+++ src/models/Consumer.ts
@@ -1,18 +1,41 @@
 import IAgent from "./IAgent";
 
+export interface Preference {
+    producerId: number;
+    weight: number;
+}
+
 export default class Consumer implements IAgent {
     income: number;
     spending: number;
     propensityToConsume: number;
+    priceIndex: number;
+    preferences: Preference[];
 
-    constructor(income: number, spending: number, propensityToConsume: number) {
+    constructor(income: number, spending: number, propensityToConsume: number, priceIndex: number, preferences: Preference[]) {
         this.income = income;
         this.spending = spending;
         this.propensityToConsume = propensityToConsume;
+        this.priceIndex = priceIndex;
+        this.preferences = preferences;
     }
+
     update(): void {
         // Update the consumer's behavior based on market conditions
         this.calculateSpending();
+        this.updateIncome();
     }
+
+    calculateSpending(): void {
+        const totalSpending = (this.income * this.propensityToConsume) / this.priceIndex;
+        this.spending = this.preferences.reduce((total, preference) => {
+          return total + totalSpending * preference.weight;
+        }, 0);
+      }
+
+    updateIncome(): void {
+        // Adjust the consumer's income based on changes in the economy or employment status
+      }
 }
+
 
