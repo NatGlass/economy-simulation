f84e614f310ceb6b9981cce171f548204cef31ec
Nathan G <Nathanglass26@gmail.com>
2023-05-08 10:26:47 +0100

    adjust income funcions

--- src/models/Consumer.ts
+++ src/models/Consumer.ts
@@ -35,6 +35,35 @@ export default class Consumer implements IAgent {
 
     updateIncome(): void {
         // Adjust the consumer's income based on changes in the economy or employment status
+        this.adjustIncomeForEmploymentStatus();
+        this.adjustIncomeForSalaryRaise();
+        this.adjustIncomeForInflation();
+      }
+
+    adjustIncomeForEmploymentStatus(): void {
+        // Simulate changes in employment status
+        const isEmployed = Math.random() < 0.95; // 95% chance of being employed
+        if (isEmployed) {
+          const baseIncome = 20000; // Base income for employed consumers
+          this.income = baseIncome;
+        } else {
+          this.income = 0;
+        }
+      }
+
+      adjustIncomeForSalaryRaise(): void {
+        // Simulate salary raises and promotions
+        const hasSalaryRaise = Math.random() < 0.1; // 10% chance of receiving a salary raise
+        if (hasSalaryRaise) {
+          const salaryRaisePercentage = Math.random() * (15 - 5) + 5; // between 5-15% salary raise
+          this.income *= 1 + salaryRaisePercentage;
+        }
+      }
+
+      adjustIncomeForInflation(): void {
+        // Simulate the effect of inflation on consumer income
+        const inflationRate = 0.02; // Base inflation rate of 2%
+        this.income /= 1 + inflationRate;
       }
 }
 
