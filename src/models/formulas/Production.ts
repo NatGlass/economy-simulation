18196ff0367127f074c4685db88cfcc1e7b1e039
Nathan G <Nathanglass26@gmail.com>
2023-05-08 10:36:30 +0100

    Cobb Douglas Production Formula

---
+++ src/models/formulas/Production.ts
@@ -0,0 +1,24 @@
+// Cobb Douglas production function formula
+// Y = A * (L ^ alpha) * (K ^ beta)
+// Y = output
+// A = total factor productivity
+// L = labour input
+// K = capital input
+// alpha = labour elasticity
+// beta = capital elasticity
+
+export default class ProductionFormulae{
+    static calculateOutput(
+        totalFactorProductivity: number,
+        labour: number,
+        capital: number,
+        labourElasticity: number,
+        capitalElasticity: number
+    ): number {
+        const labourTerm = Math.pow(labour, labourElasticity);
+        const capitalTerm = Math.pow(capital, capitalElasticity);
+
+        const output = totalFactorProductivity * labourTerm * capitalTerm;
+        return output;
+    }
+}
